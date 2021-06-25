679e59cb3b579bc7e242ab62aae5c4809c584421
wxi <45331556+cnwxi@users.noreply.github.com>
2021-06-25 21:01:44 +0800

    精简

--- final/myknn.py
+++ final/myknn.py
@@ -4,13 +4,11 @@ from collections import Counter
 
 def KNN(matTrain, y_train, matTest, k=50):
     numTrain = matTrain.shape[0]
-    numTest = matTest.shape[0]
-    for i in range(numTest):
-        distances = [np.linalg.norm(matTrain[i] - matTest) for i in range(numTrain)]
-        # print(distances)
-        distances = np.array(distances)
-        nearest = distances.argsort()  # 返回原数组的下标
-        topK_y = [y_train[i] for i in nearest[:k]]  # topK_y得到最近的k个点所属的类别（很多个）
-        votes = Counter(topK_y)
-        predict_y = votes.most_common(1)[0][0]  # 多个类别中的大多数类别
+    distances = [np.linalg.norm(matTrain[i] - matTest) for i in range(numTrain)]
+    # print(distances)
+    distances = np.array(distances)
+    nearest = distances.argsort()
+    topK_y = [y_train[i] for i in nearest[:k]]
+    votes = Counter(topK_y)
+    predict_y = votes.most_common(1)[0][0]
     return predict_y
